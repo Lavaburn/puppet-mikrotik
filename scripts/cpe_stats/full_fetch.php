e24913dc68da1abf747310a1230daec2940d0396
Nicolas Truyens <nicolas@truyens.com>
2017-03-06 14:49:44 +0100

    Restart checking if in RUNNING state for 1 day.
    Change-Id: Id68845fc8554156a3415eefcda61047b0bbcc910

--- scripts/cpe_stats/full_fetch.php
+++ scripts/cpe_stats/full_fetch.php
@@ -10,23 +10,24 @@ require_once(ROOT."/util/LogUtil.class.inc");
 
 $bsr = $DB->query("SELECT * FROM bsr_check");
 while ($row = $bsr->fetch_assoc()) {
-  if ($row["snmp_last_result"] == "RUNNING") {
-    LogUtil::warning(MODULE, "The check for BSR ".$row["name"]." is still running. Will not start a new instance now!");
-
-    if (time() - strtotime($row["snmp_last_check"]) > 24*60*60) {
-      //Last valid check is more than 1 day old!
-      LogUtil::critical(MODULE, "The last SNMP check for BSR ".$row["name"]." is more than 1 day old!");
-      //TODO SENSU ALERT ?
-      //TODO MAIL ??
-    }
-  } else {
-     if (time() - strtotime($row["snmp_last_check"]) < MIN_CHECK_INTERVAL * 60) {
-       //Previous check was less than x minutes ago
-       LogUtil::warning(MODULE, "Previous check for BSR ".$row["name"]." was at ".$row["snmp_last_check"].". Will not start a new check now!");
-     } else {
-       exec("nohup ".ROOT."/scripts/cpe_stats/bsr_fetch.php ".$row["ip"]." > /dev/null 2>&1 &");
-     }
-  }
+ 	$execute_check = false;
+ 	if ($row["snmp_last_result"] == "RUNNING") {
+ 		if (time() - strtotime($row["snmp_last_check"]) > 24*60*60) {//TODO MAX_CHECK_INTERVAL = 24*60 //minutes
+ 			$execute_check = true;
+ 		} else {
+ 			LogUtil::warning(MODULE, "The check for Base Station ".$row["name"]." is still running. Please wait until check is completed.");
+ 		}
+ 	} else {
+ 		if (time() - strtotime($row["snmp_last_check"]) < MIN_CHECK_INTERVAL*60) {
+ 			LogUtil::warning(MODULE, "Previous check for Base Station ".$row["name"]." was at ".$row["snmp_last_check"].". Please wait ".MIN_CHECK_INTERVAL." minutes before retrying.");
+ 		} else {
+ 			$execute_check = true;
+ 		}
+ 	}
+	 
+ 	if ($execute_check) {
+ 		exec("nohup ".ROOT."/scripts/cpe_stats/bsr_fetch.php ".$row["ip"]." > /dev/null 2>&1 &");
+ 	}
 }
 $bsr->free();    
 
