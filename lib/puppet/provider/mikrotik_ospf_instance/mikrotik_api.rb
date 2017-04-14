b3ef78f778d42b5dba5c60df5f768a76116ca781
Nicolas Truyens <nicolas@truyens.com>
2017-04-14 11:29:50 +0200

    Added params for OSPF instance

--- lib/puppet/provider/mikrotik_ospf_instance/mikrotik_api.rb
+++ lib/puppet/provider/mikrotik_ospf_instance/mikrotik_api.rb
@@ -12,29 +12,48 @@ Puppet::Type.type(:mikrotik_ospf_instance).provide(:mikrotik_api, :parent => Pup
   end
   
   def self.ospfInstance(data)
-      new(
-        :ensure                 => :present,
-        :name                   => data['name'],
-        :router_id              => data['router-id'],
-        :distribute_default     => data['distribute-default'],
-        :redistribute_connected => data['redistribute-connected'],
-        :redistribute_static    => data['redistribute-static'],
-        :redistribute_ospf      => data['redistribute-other-ospf'],
-        :redistribute_bgp       => data['redistribute-bgp'],
-        :in_filter              => data['in-filter'],
-        :out_filter             => data['out-filter'],
-        :metric_default         => data['metric-default'],
-        :metric_connected       => data['metric-connected'],
-        :metric_static          => data['metric-static'],
-        :metric_ospf            => data['metric-other-ospf'],
-        :metric_bgp             => data['metric-bgp']
-      )
+    if data['disabled'] == "true"
+      state = :disabled
+    else
+      state = :enabled
+    end
+    
+    new(
+      :ensure                 => :present,
+      :state                  => state,
+      :name                   => data['name'],
+      :router_id              => data['router-id'],
+      :distribute_default     => data['distribute-default'],
+      :redistribute_connected => data['redistribute-connected'],
+      :redistribute_static    => data['redistribute-static'],
+      :redistribute_ospf      => data['redistribute-other-ospf'],
+      :redistribute_bgp       => data['redistribute-bgp'],
+      :redistribute_rip       => data['redistribute-rip'],
+      :in_filter              => data['in-filter'],
+      :out_filter             => data['out-filter'],
+      :metric_default         => data['metric-default'],
+      :metric_connected       => data['metric-connected'],
+      :metric_static          => data['metric-static'],
+      :metric_ospf            => data['metric-other-ospf'],
+      :metric_bgp             => data['metric-bgp'],
+      :metric_rip             => data['metric-rip'],
+      :mpls_te_area           => data['mpls-te-area'],
+      :mpls_te_router_id      => data['mpls-te-router-id'],
+      :routing_table          => data['routing-table']
+    )
   end
 
   def flush
     Puppet.debug("Flushing OSPF Instance #{resource[:name]}")
       
     params = {}
+      
+    if @property_hash[:state] == :disabled
+      params["disabled"] = true
+    elsif @property_hash[:state] == :enabled
+      params["disabled"] = false
+    end
+
     params["name"] = resource[:name]
     params["router-id"] = resource[:router_id] if ! resource[:router_id].nil?
     params["distribute-default"] = resource[:distribute_default] if ! resource[:distribute_default].nil?
@@ -42,6 +61,7 @@ Puppet::Type.type(:mikrotik_ospf_instance).provide(:mikrotik_api, :parent => Pup
     params["redistribute-static"] = resource[:redistribute_static] if ! resource[:redistribute_static].nil?
     params["redistribute-other-ospf"] = resource[:redistribute_ospf] if ! resource[:redistribute_ospf].nil?
     params["redistribute-bgp"] = resource[:redistribute_bgp] if ! resource[:redistribute_bgp].nil?
+    params["redistribute-rip"] = resource[:redistribute_rip] if ! resource[:redistribute_rip].nil?
     params["in-filter"] = resource[:in_filter] if ! resource[:in_filter].nil?
     params["out-filter"] = resource[:out_filter] if ! resource[:out_filter].nil?
     params["metric-default"] = resource[:metric_default] if ! resource[:metric_default].nil?
@@ -49,6 +69,10 @@ Puppet::Type.type(:mikrotik_ospf_instance).provide(:mikrotik_api, :parent => Pup
     params["metric-static"] = resource[:metric_static] if ! resource[:metric_static].nil?
     params["metric-bgp"] = resource[:metric_bgp] if ! resource[:metric_bgp].nil?
     params["metric-other-ospf"] = resource[:metric_ospf] if ! resource[:metric_ospf].nil?
+    params["metric-rip"] = resource[:metric_rip] if ! resource[:metric_rip].nil?
+    params["mpls-te-area"] = resource[:mpls_te_area] if ! resource[:mpls_te_area].nil?
+    params["mpls-te-router-id"] = resource[:mpls_te_router_id] if ! resource[:mpls_te_router_id].nil?
+    params["routing-table"] = resource[:routing_table] if ! resource[:routing_table].nil?
 
     lookup = {}
     lookup["name"] = resource[:name]
--- lib/puppet/type/mikrotik_ospf_instance.rb
+++ lib/puppet/type/mikrotik_ospf_instance.rb
@@ -1,9 +1,43 @@
 Puppet::Type.newtype(:mikrotik_ospf_instance) do
   apply_to_all
-  
+
   ensurable do
-    defaultvalues
     defaultto :present
+    
+    newvalue(:present) do
+      provider.create  
+    end
+    
+    newvalue(:absent) do
+      provider.destroy
+    end
+    
+    newvalue(:enabled) do
+      provider.setState(:enabled)      
+    end
+
+    newvalue(:disabled) do
+      provider.setState(:disabled)
+    end
+
+    def retrieve
+      provider.getState
+    end
+    
+    def insync?(is)
+      @should.each { |should| 
+        case should
+          when :present
+            return (provider.getState != :absent)
+          when :absent
+            return (provider.getState == :absent)
+          when :enabled                   
+            return (provider.getState == :enabled)
+          when :disabled                      
+            return (provider.getState == :disabled)       
+        end
+      }
+    end
   end
   
   newparam(:name) do
@@ -39,6 +73,11 @@ Puppet::Type.newtype(:mikrotik_ospf_instance) do
     desc 'Whether to redistribute BGP routes to OSPF.'
     newvalues('no', 'as-type-1', 'as-type-2')
   end
+  
+  newproperty(:redistribute_rip) do
+    desc 'Whether to redistribute RIP routes to OSPF.'
+    newvalues('no', 'as-type-1', 'as-type-2')
+  end
 
   newproperty(:in_filter) do
     desc 'The input filter applying on this instance.'
@@ -66,5 +105,21 @@ Puppet::Type.newtype(:mikrotik_ospf_instance) do
 
   newproperty(:metric_ospf) do
     desc 'The metric to use when redistributing OSPF routes from other instances on this instance.'
+  end
+  
+  newproperty(:metric_rip) do
+    desc 'The metric to use when redistributing RIP routes on this instance.'
+  end
+
+  newproperty(:mpls_te_area) do
+    desc 'MPLS Traffic Engineering Area ID'
+  end
+  
+  newproperty(:mpls_te_router_id) do
+    desc 'MPLS Traffic Engineering Router ID'
+  end
+  
+  newproperty(:routing_table) do
+    desc 'Routing Table to receive and install routes from/to'
   end
 end
